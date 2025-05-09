ad699ffbb80b35032328815de11e6a46e52de7d2
armagonthefirst <shariq2@live.com>
2025-05-09 15:36:37 +1000

    loading screen plus little css fix
    ^^

--- app/database/softcons_queries.py
+++ app/database/softcons_queries.py
@@ -4,13 +4,20 @@ class SoftConstraint(db.Model):
     __tablename__ = 'soft_constraints'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    gpa_penalty = db.Column(db.Integer)
-    wellbeing_penalty = db.Column(db.Integer)
-    bully_penalty = db.Column(db.Integer)
-    influence_weight = db.Column(db.Integer)
-    isolation_weight = db.Column(db.Integer)
-    friend_score_toggle = db.Column(db.Boolean)
-    friend_balance_toggle = db.Column(db.Boolean)
-    min_friends = db.Column(db.Integer)
-    priority_order = db.Column(db.Text)  # Store CSV string or JSON string
     timestamp = db.Column(db.DateTime, server_default=db.func.now())
+
+    gpa_penalty_weight = db.Column(db.Integer)
+    wellbeing_penalty_weight = db.Column(db.Integer)
+    bully_penalty_weight = db.Column(db.Integer)
+    influence_std_weight = db.Column(db.Integer)
+    isolated_std_weight = db.Column(db.Integer)
+
+    min_friends_required = db.Column(db.Integer)
+    friendship_score_weight = db.Column(db.Integer)
+    friendship_balance_weight = db.Column(db.Integer)
+
+    prioritize_academic = db.Column(db.Integer)
+    prioritize_wellbeing = db.Column(db.Integer)
+    prioritize_bullying = db.Column(db.Integer)
+    prioritize_social_influence = db.Column(db.Integer)
+    prioritize_friendship = db.Column(db.Integer)
